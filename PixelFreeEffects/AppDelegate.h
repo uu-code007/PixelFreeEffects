17a21d2287cae1cdd1cd2587823ad1c548d7a496
sunmu <musun@faceunity.com>
2020-09-03 17:02:27 +0800

    Initial Commit

---
+++ PixelFreeEffects/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PixelFreeEffects
+//
+//  Created by 孙慕 on 2020/9/3.
+//  Copyright © 2020 孙慕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PixelFreeEffects/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  PixelFreeEffects
+//
+//  Created by 孙慕 on 2020/9/3.
+//  Copyright © 2020 孙慕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
