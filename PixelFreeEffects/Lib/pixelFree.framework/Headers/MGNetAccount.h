0364cf2a110275098ec500eaa1bf36dee62d75cc
sunmu <musun@faceunity.com>
2020-09-24 09:35:47 +0800

    第一次会鉴权失败

--- PixelFreeEffects/Lib/pixelFree.framework/Headers/MGNetAccount.h
+++ PixelFreeEffects/Lib/pixelFree.framework/Headers/MGNetAccount.h
@@ -14,10 +14,10 @@
 //#define MG_LICENSE_KEY      @"JWpTbbDbisyyTeFUf_NeGo2NmCJhhJn9" // api_key        测试 KEY
 //#define MG_LICENSE_SECRET    @"uf0Sh3CAKT7VhYNwzsuvUX0WGT_PPSzc" // api_secret
 
-#define MG_LICENSE_KEY      @"kVR7_Z8lZR9_uaTUBEGJQnH3pOuewSZq" // api_key        测试 KEY
-#define MG_LICENSE_SECRET    @"myoWhqFFgEPFm67jMm5bhLRbE6SkH4Tp" // api_secret
+//#define MG_LICENSE_KEY      @"kVR7_Z8lZR9_uaTUBEGJQnH3pOuewSZq" // api_key        测试 KEY
+//#define MG_LICENSE_SECRET    @"myoWhqFFgEPFm67jMm5bhLRbE6SkH4Tp" // api_secret
 
-//#define MG_LICENSE_KEY      @"UJ8Sq132vsqyy9ATQiSoG9TY6R311f_4" // api_key          测试 KEY
-//#define MG_LICENSE_SECRET    @"DTKTxasC05lqZ6r38Be6iRYI7h2VLDjg" // api_secret
+#define MG_LICENSE_KEY      @"UJ8Sq132vsqyy9ATQiSoG9TY6R311f_4" // api_key          测试 KEY
+#define MG_LICENSE_SECRET    @"DTKTxasC05lqZ6r38Be6iRYI7h2VLDjg" // api_secret
 
 #endif /* MGNetAccount_example_h */
