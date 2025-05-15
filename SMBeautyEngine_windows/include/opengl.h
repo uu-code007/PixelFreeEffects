ff1e5251dab2a2fea607fd9eaa3acb836bc0dd5d
uu-code007 <ios_sunmu@icloud.com>
2025-05-15 10:11:24 +0800

    aa

--- SMBeautyEngine_windows/include/opengl.h
+++ SMBeautyEngine_windows/include/opengl.h
@@ -32,6 +32,7 @@
 #include <windows.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #elif __APPLE__
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
